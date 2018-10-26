21ed97a98981f16f0eb39b36c4b8ff42ab787a3c
Chetan Chawla <chchawla@yahoo.co.in>
2018-10-27 01:40:30 +0530

    Final Working Code with Networking Part

--- Python Code for connecting to Firebase.py
+++ Python Code for connecting to Firebase.py
@@ -4,6 +4,9 @@ import os
 import glob
 import time
 
+
+selfID="Car1" ##this is the dummy for the self's Number Plate unique ID
+
 config={        #fill according to your firebase database
     "apiKey": " AIzaSyAPumd637Cml2lncPQWARhnE0oei_AsyeU ",
     "authDomain": "overtake-5888b.firebaseapp.com",
@@ -12,22 +15,46 @@ config={        #fill according to your firebase database
 }
 firebase =pyrebase.initialize_app(config)
 db=firebase.database()
-groups=db.child("Groups").get()
-print(groups.val())
+#For turning LED Green/Red On or Off
+import RPi.GPIO as GPIO 
+GPIO.setmode(GPIO.BOARD)
+mypinG = 11
+mypinR = 12
+GPIO.setup(mypinG, GPIO.OUT, initial = 0)
+GPIO.setup(mypinR, GPIO.OUT, initial = 0)
+overtake=1
 
-
-
-'''def uploadMess(tuSun,yeSun):
-    db.child(tuSun).update(yeSun)
-    
-
-my_stream = db.child("database1").child(ting).child("Message").stream(stream_handler)
-while True:          
-        data1=''
-        forQuit=''
-        data=''
-        while 1:
-                try:
-                    dataone=({'Lat':Lat,'Long':Long,'Alt':Alt,'Speed':Speed})
-                    db.child("database1").child(ting).child("Location").update(dataone)       
-'''
+def uploadMess(tuSun,yeSun):
+    db.child("Groups").child(tuSun).update(yeSun)
+def stream_handler(message):
+    print(message["data"])
+    overtake=int(message["data"])
+    if(overtake==0):
+        GPIO.output(mypinG, 1)
+        GPIO.output(mypinR, 0)
+        print("Overtake")
+    else:
+        GPIO.output(mypinG, 0)
+        GPIO.output(mypinR, 1)
+        print("Dont Overtake")
+def getPlateID():
+    ###Here goes the detction code for Number Plate
+    ###Number Plate Detection Code Returns a number- Plate which is the unique id for the detected car
+    return 'Car2'
+def PlatePresent():
+    if(getPlateID!=''):
+        return 1
+    else:
+        return 0
+#print("Listening to stream")
+#my_stream = db.child("Cars").child(Plate).stream(stream_handler)
+PlateTemp='Temp'
+while(True):
+    ######
+    #checkForPlate
+    ######
+    if(PlatePresent()!=0):
+        Plate=getPlateID();
+        if(Plate!=PlateTemp):
+            PlateTemp=Plate
+            my_stream = db.child("Cars").child(Plate).stream(stream_handler)
